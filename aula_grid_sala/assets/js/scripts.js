a8bd5b91b2df326bc482aac8d331f2674850f3db
LucasVicentee <lh201612@gmail.com>
2025-10-14 17:29:34 -0300

    Adicionando a página de praias com flex grid

---
+++ aula_grid_sala/assets/js/scripts.js
@@ -0,0 +1,9 @@
+function mostrarDescricao(idDescricao) {
+    const descricao = document.getElementById(idDescricao);
+    if (descricao.style.display === "none" || descricao.style.display === "") {
+        descricao.style.display = "block";
+    } 
+    else {
+        descricao.style.display = "none";
+    }
+}
